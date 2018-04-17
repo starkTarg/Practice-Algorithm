3c01dd23cde887dc7ee425d0c2f31bc80ee65312
tausif <tausif@192.168.1.6>
2018-04-17 09:18:19 +0530

    First commit

---
+++ src/com/practice/array_string/ArrayRepresentPreorderNextGreaterElement.java
@@ -0,0 +1,9 @@
+package com.practice.array_string;
+
+// program for an efficient solution to check if
+//a given array can represent Preorder traversal of
+//a Binary Search Tree
+// https://www.geeksforgeeks.org/check-if-a-given-array-can-represent-preorder-traversal-of-binary-search-tree/
+public class ArrayRepresentPreorderNextGreaterElement {
+
+}
---
+++ src/com/practice/array_string/KthSmallestLargestQuickSelect.java
@@ -0,0 +1,8 @@
+package com.practice.array_string;
+
+// code for kth smallest element in an array
+// https://www.geeksforgeeks.org/kth-smallestlargest-element-unsorted-array/
+
+public class KthSmallestLargestQuickSelect {
+
+}
